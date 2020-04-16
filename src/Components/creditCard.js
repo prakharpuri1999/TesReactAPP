362f13a241419710ce70039637b16c9deb7291b1
vikas <vikasprashar99@gmail.com>
2020-04-16 20:26:17 +0530

    updates

--- src/Components/creditCard.js
+++ src/Components/creditCard.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import Button from 'react-bootstrap/Button'
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form'
-
+// import MainScreen from './screens/MainScreen';
+import MainScreen from '../screens/MainScreen'
 class creditCard extends Component {
   
   
@@ -17,6 +18,7 @@ class creditCard extends Component {
         return (
           <div className="text-left">
     <h1>Creediit card</h1>
+    <MainScreen />
         </div>
 );
     }
